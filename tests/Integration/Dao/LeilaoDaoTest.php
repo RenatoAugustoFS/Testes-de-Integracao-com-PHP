12c4585622a00a96676435a6b9e9dfed0a5a9281
Renato Augusto <renatoaugusto.ads@gmail.com>
2021-08-18 09:03:43 -0300

    Criado testes para leiloes finalizados e nao finalizados usando o padrão AAA - ARRANGE ACT & ASSERT

--- tests/Integration/Dao/LeilaoDaoTest.php
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -29,12 +29,38 @@ class LeilaoDaoTest extends TestCase
         self::$pdo->beginTransaction();
     }
 
-    public function testInsercaoEBuscaDeveFuncionar()
+    /**
+     * @dataProvider leiloes
+     */
+    public function testBuscaLeiloesFinalizados(array $leiloes)
     {
         //Arrange
-        $leilao = new Leilao('Variante 0km');
         $leilaoDao = new LeilaoDao(self::$pdo);
-        $leilaoDao->salva($leilao);
+
+        foreach ($leiloes as $leilao){
+            $leilaoDao->salva($leilao);
+        }
+
+        //Act
+        $leiloes = $leilaoDao->recuperarFinalizados();
+
+        //Assert
+        self::assertCount(1, $leiloes);
+        self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
+        self::assertSame('Fiat 147', $leiloes[0]->recuperarDescricao());
+    }
+
+    /**
+     * @dataProvider leiloes
+     */
+    public function testBuscaLeiloesNaoFinalizados(array $leiloes)
+    {
+        //Arrange
+        $leilaoDao = new LeilaoDao(self::$pdo);
+
+        foreach ($leiloes as $leilao){
+            $leilaoDao->salva($leilao);
+        }
 
         //Act
         $leiloes = $leilaoDao->recuperarNaoFinalizados();
@@ -43,6 +69,20 @@ class LeilaoDaoTest extends TestCase
         self::assertCount(1, $leiloes);
         self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
         self::assertSame('Variante 0km', $leiloes[0]->recuperarDescricao());
+    }
+
+    public function leiloes()
+    {
+        $leilaoNaoFinalizado = new Leilao('Variante 0km');
+
+        $leilaoFinalizado = new Leilao('Fiat 147');
+        $leilaoFinalizado->finaliza();
+
+        return [
+            [
+                [$leilaoNaoFinalizado, $leilaoFinalizado]
+            ]
+        ];
     }
 
     protected function tearDown(): void
